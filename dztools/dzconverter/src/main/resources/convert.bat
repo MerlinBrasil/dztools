057291173b04a7584b9bc5e4a43b28d0517a16a6
Juergen Reiss <juergen.reiss@gmx.de>
2014-12-27 14:23:10 +0100

    Added filtering to batch script.

--- dztools/dzconverter/src/main/resources/convert.bat
+++ dztools/dzconverter/src/main/resources/convert.bat
@@ -4,4 +4,4 @@ cscript //E:JScript //nologo "%~f0"
 exit/b
 */
 var obj = new ActiveXObject("WScript.Shell");
-obj.SendKeys("java -jar dzconverter-0.9.0.jar ");
+obj.SendKeys("java -jar dzconverter-${project.version}.jar ");
