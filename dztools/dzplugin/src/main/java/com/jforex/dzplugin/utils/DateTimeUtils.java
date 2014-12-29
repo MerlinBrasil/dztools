321749343396d36e7ac8adc65b4a73551dcdc25f
Juergen Reiss <juergen.reiss@gmx.de>
2014-12-29 12:08:40 +0100

    Changed log level for GMT timeout.

--- dztools/dzplugin/src/main/java/com/jforex/dzplugin/utils/DateTimeUtils.java
+++ dztools/dzplugin/src/main/java/com/jforex/dzplugin/utils/DateTimeUtils.java
@@ -12,12 +12,12 @@ package com.jforex.dzplugin.utils;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
@@ -37,12 +37,13 @@ import org.apache.commons.net.ntp.TimeInfo;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import com.jforex.dzplugin.ZorroLogger;
+import com.jforex.dzplugin.config.Configuration;
+
 import com.dukascopy.api.IDataService;
 import com.dukascopy.api.ITimeDomain;
 import com.dukascopy.api.JFException;
 import com.dukascopy.api.Period;
-import com.jforex.dzplugin.ZorroLogger;
-import com.jforex.dzplugin.config.Configuration;
 
 public class DateTimeUtils {
 
@@ -96,7 +97,7 @@ public class DateTimeUtils {
             TimeInfo timeInfo = timeClient.getTime(inetAddress);
             return timeInfo.getMessage().getTransmitTimeStamp().getTime();
         } catch (IOException e) {
-            ZorroLogger.indicateError(logger, "Unable to get GMT time: " + e.getMessage());
+            logger.warn("Unable to get GMT time: " + e.getMessage());
         }
         logger.warn("Using System time now!");
         return System.currentTimeMillis();
