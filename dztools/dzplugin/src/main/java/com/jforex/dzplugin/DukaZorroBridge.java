b7293c1114e56da0c2e72014233a4827e3bd6d0d
Juergen Reiss <juergen.reiss@gmx.de>
2014-12-29 12:01:33 +0100

    Changed log level.

--- dztools/dzplugin/src/main/java/com/jforex/dzplugin/DukaZorroBridge.java
+++ dztools/dzplugin/src/main/java/com/jforex/dzplugin/DukaZorroBridge.java
@@ -10,12 +10,12 @@ package com.jforex.dzplugin;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
@@ -179,7 +179,7 @@ public class DukaZorroBridge {
         if (subscriptionResult == ReturnCodes.ASSET_AVAILABLE)
             priceEngine.initInstruments(instruments);
 
-        logger.debug("Subscription for " + toSubscribeInstrument + " successful.");
+        logger.info("Subscription for " + toSubscribeInstrument + " successful.");
         return subscriptionResult;
     }
 
