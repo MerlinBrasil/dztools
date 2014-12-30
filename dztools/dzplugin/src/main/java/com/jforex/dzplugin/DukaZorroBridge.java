30c72ebe2e4ebe4bd9e226717ae5648c0b845595
Juergen Reiss <juergen.reiss@gmx.de>
2014-12-30 17:03:54 +0100

    Added DLL log prefix.

--- dztools/dzplugin/src/main/java/com/jforex/dzplugin/DukaZorroBridge.java
+++ dztools/dzplugin/src/main/java/com/jforex/dzplugin/DukaZorroBridge.java
@@ -10,12 +10,12 @@ package com.jforex.dzplugin;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
@@ -183,30 +183,6 @@ public class DukaZorroBridge {
     }
 
     public void doDLLlog(String msg) {
-        logger.info(msg);
-    }
-
-    public IClient getClient() {
-        return client;
-    }
-
-    public IContext getContext() {
-        return context;
-    }
-
-    public AccountInfo getAccountInfo() {
-        return accountInfo;
-    }
-
-    public IPriceEngine getPriceEngine() {
-        return priceEngine;
-    }
-
-    public DateTimeUtils getDateTimeUtils() {
-        return dateTimeUtils;
-    }
-
-    public ServerTimeProvider getServerTimeProvider() {
-        return serverTimeProvider;
+        logger.info("DLL msg " + msg);
     }
 }
